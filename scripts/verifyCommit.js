8da0492f33f140a4b35e9d22fc906710ae6c0712
Hongzf <1628415507@qq.com>
2022-10-08 14:50:23 +0800

    feat：husky配置

---
+++ scripts/verifyCommit.js
@@ -0,0 +1,25 @@
+/*
+ * @Author: Hongzf
+ * @Date: 2022-10-08 14:31:24
+ * @LastEditors: Hongzf
+ * @LastEditTime: 2022-10-08 14:41:02
+ * @Description: 
+ */
+const msg = require('fs').readFileSync('.git/COMMIT_EDITMSG', 'utf-8').trim()
+// 验证在GitHub中的提交⽇志都符合type(scope): message 的格式
+const commitRE = /^(revert: )?(feat|fix|docs|dx|style|refactor|perf|test|workflow|build|ci|chore|types|wip|release)(\(.+\))?: .{1,50}/
+const mergeRe = /^(Merge pull request|Merge branch)/
+
+if (!commitRE.test(msg)) {
+    if (!mergeRe.test(msg)) {
+        console.log('git commit信息校验不通过')
+        console.error(`git commit的信息格式不对, 需要使用 title(scope): desc的格式
+      比如 fix: xxbug
+      feat(test): add new 
+      具体校验逻辑看 scripts/verifyCommit.js
+    `)
+        process.exit(1)
+    }
+} else {
+    console.log('git commit信息校验通过')
+}
